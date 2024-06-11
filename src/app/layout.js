b53725127487a45a5dfa8716d830c03e318a221b
Nataliia <natalabojko@gmail.com>
2024-06-11 16:56:41 +0300

    start global settings

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
