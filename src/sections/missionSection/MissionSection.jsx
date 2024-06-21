1049311e7b9628a678eb54adfb1d3aa70381dce5
Maksimchik36 <Peltek1985@gmail.com>
2024-06-22 00:59:56 +0300

    completed mission mobile, changed some global styles

--- src/sections/missionSection/MissionSection.jsx
+++ src/sections/missionSection/MissionSection.jsx
@@ -1,9 +1,33 @@
+"use client"
 import React from 'react'
+import { CldImage } from 'next-cloudinary'
+import styles from './MissionSection.module.scss'
+
 
 const MissionSection = () => {
     return (
-        <section>Mission Section</section>
+        <section className="topSection">
+            <div className="container">
+                <h1 className="sectionTitle">Місія та статут</h1>
+                <p className={styles.subTitle}>Фонд «Перемоги та Відновлення» – це про майбутнє. Щасливе, комфортне та справедливе.</p>
+                <div className={styles.imgWrapper}>
+                    <CldImage
+                        className={styles.img}
+                        fill
+                        src='missionstatute_uji6oq'
+                        sizes='100vw'
+                        alt='Monument of Independence in Kyiv'
+                    />
+                </div>
+                <p className={styles.text}>Благодійний Фонд «<span className={styles.accentText}>Перемоги та Відновлення</span>» — це динамічна неприбуткова організація, яка прагне відновити Україну та зміцнити її державність. </p>
+                <p className={styles.text}><span className={styles.accentText}>Ми допомагаємо українському народу</span> досягти перемоги та створюємо комфортні умови для всіх. Наша головна діяльність зосереджена на психосоціальній підтримці та культурно-просвітницькій роботі, відкритій для всіх, хто відчуває в цьому потребу.</p>
+                <p className={styles.text}><span className={styles.accentText}>Ми працюємо з людьми</span>, постраждалими від воєнних дій, та тими, хто потребує соціальної адаптації, приділяючи особливу увагу студентам і молоді, адже вважаємо психологічну підтримку надважливою для майбутнього країни.</p>
+                <p className={styles.text}>Наші проєкти <span className={styles.accentText}>охоплюють наукові та освітні заходи</span>, популяризацію здоров’я, саморозвитку та самореалізації через соціальні мережі та публічні виступи. Фонд створює платформи для нетворкінгу, активно інвестує у перемогу країни, організовуючи збори, пожертви та волонтерство, щоб допомогти державі у всіх необхідних напрямках.</p>
+                <button className={styles.btn}>Ознайомитись зі Статутом</button>
+            </div>
+        </section>
     )
 }
+
 
 export default MissionSection
