9c095f38be54c98bb9f0065d8526d1ff199d62e5
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-06-26 21:52:17 +0300

    finish ProjectPage

---
+++ src/data/projectsCategories.data.js
@@ -0,0 +1,20 @@
+export const projectsCategories = [
+  {
+    id: 1,
+    stateTitle: 'Announced',
+    title: 'Анонсовані проєкти',
+    titleEn: 'Announced projects',
+  },
+  {
+    id: 2,
+    stateTitle: 'Current',
+    title: 'Поточні проєкти',
+    titleEn: 'Current projects',
+  },
+  {
+    id: 3,
+    stateTitle: 'Completed',
+    title: 'Реалізовані проєкти',
+    titleEn: 'Implemented projects',
+  },
+];
