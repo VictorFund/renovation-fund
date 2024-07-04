c7c888691b0d8eb735b71308a34994d367ab7ff6
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2024-07-04 14:54:17 +0300

    add PartnerForm

--- src/sections/partnershipSection/PartnershipSection.jsx
+++ src/sections/partnershipSection/PartnershipSection.jsx
@@ -1,88 +1,135 @@
-'use client';
-import styles from './PartnershipSection.module.scss';
-import { GetDataWithPathname } from '@/fetch/clientFetch';
-import { CldImage } from 'next-cloudinary';
+"use client";
+
+import { CldImage } from "next-cloudinary";
+import { GetDataWithPathname } from "@/fetch/clientFetch";
+import PartnerForm from "@/components/Forms/PartnerForm";
+
+import styles from "./PartnershipSection.module.scss";
 
 const PartnershipSection = () => {
-  const { data } = GetDataWithPathname('partnership');
-  // console.log('data', data);
+    const { data } = GetDataWithPathname("partnership");
+    // console.log('data', data);
 
-  return (
-    <section className="topSection">
-      <div className={`container`}>
-        <h1 className={`sectionTitle ${styles.title}`}>Партнерство</h1>
+    return (
+        <section className='topSection'>
+            <div className={`container`}>
+                <h1 className={`sectionTitle ${styles.title}`}>Партнерство</h1>
 
-        <ul className={styles.partnershipList}>
-          {data?.map(
-            ({ slug, title, siteLink, logo, isApproved, isMainPartner }) => {
-              if (isApproved) {
-                if (isMainPartner) {
-                  return (
-                    <li key={slug}>
-                      <a
-                        href={siteLink}
-                        target="_blank"
-                        aria-label={title}
-                        className={styles.partnershipItem}
-                      >
-                        <figure className={styles.imgContainer}>
-                          <CldImage
-                            src={logo}
-                            alt={title}
-                            fill={true}
-                            sizes="(max-width: 768px) 128px, (max-width: 1440px) 100px"
-                          />
-                        </figure>
-                        <h3 className={styles.partnershipTitle}>{title}</h3>
-                        <p className={styles.btn}>
-                          <span>Детальніше</span>
-                          <svg className={styles.arrow}>
-                            <use href="/sprite.svg#icon-arrow" />
-                          </svg>
-                        </p>
-                      </a>
-                    </li>
-                  );
-                }
-              }
-            }
-          )}
-        </ul>
-        <ul className={styles.partnershipSmallList}>
-          {data?.map(
-            ({ slug, title, siteLink, logo, isApproved, isMainPartner }) => {
-              if (isApproved) {
-                if (!isMainPartner) {
-                  return (
-                    <li key={slug}>
-                      <a
-                        href={siteLink}
-                        target="_blank"
-                        aria-label={title}
-                        className={styles.partnershipSmallItem}
-                      >
-                        <figure className={styles.imgSmallContainer}>
-                          <CldImage
-                            src={logo}
-                            alt={title}
-                            fill={true}
-                            sizes="(max-width: 768px) 75px"
-                          />
-                        </figure>
-                        <h3 className={styles.partnershipSmallTitle}>
-                          {title}
-                        </h3>
-                      </a>
-                    </li>
-                  );
-                }
-              }
-            }
-          )}
-        </ul>
-      </div>
-    </section>
-  );
+                <ul className={styles.partnershipList}>
+                    {data?.map(
+                        ({
+                            slug,
+                            title,
+                            siteLink,
+                            logo,
+                            isApproved,
+                            isMainPartner,
+                        }) => {
+                            if (isApproved) {
+                                if (isMainPartner) {
+                                    return (
+                                        <li key={slug}>
+                                            <a
+                                                href={siteLink}
+                                                target='_blank'
+                                                aria-label={title}
+                                                className={
+                                                    styles.partnershipItem
+                                                }
+                                            >
+                                                <figure
+                                                    className={
+                                                        styles.imgContainer
+                                                    }
+                                                >
+                                                    <CldImage
+                                                        src={logo}
+                                                        alt={title}
+                                                        fill={true}
+                                                        sizes='(max-width: 768px) 128px, (max-width: 1440px) 100px'
+                                                    />
+                                                </figure>
+                                                <h3
+                                                    className={
+                                                        styles.partnershipTitle
+                                                    }
+                                                >
+                                                    {title}
+                                                </h3>
+                                                <p className={styles.btn}>
+                                                    <span>Детальніше</span>
+                                                    <svg
+                                                        className={styles.arrow}
+                                                    >
+                                                        <use href='/sprite.svg#icon-arrow' />
+                                                    </svg>
+                                                </p>
+                                            </a>
+                                        </li>
+                                    );
+                                }
+                            }
+                        }
+                    )}
+                </ul>
+                <ul className={styles.partnershipSmallList}>
+                    {data?.map(
+                        ({
+                            slug,
+                            title,
+                            siteLink,
+                            logo,
+                            isApproved,
+                            isMainPartner,
+                        }) => {
+                            if (isApproved) {
+                                if (!isMainPartner) {
+                                    return (
+                                        <li key={slug}>
+                                            <a
+                                                href={siteLink}
+                                                target='_blank'
+                                                aria-label={title}
+                                                className={
+                                                    styles.partnershipSmallItem
+                                                }
+                                            >
+                                                <figure
+                                                    className={
+                                                        styles.imgSmallContainer
+                                                    }
+                                                >
+                                                    <CldImage
+                                                        src={logo}
+                                                        alt={title}
+                                                        fill={true}
+                                                        sizes='(max-width: 768px) 75px'
+                                                    />
+                                                </figure>
+                                                <h3
+                                                    className={
+                                                        styles.partnershipSmallTitle
+                                                    }
+                                                >
+                                                    {title}
+                                                </h3>
+                                            </a>
+                                        </li>
+                                    );
+                                }
+                            }
+                        }
+                    )}
+                </ul>
+                <div className={styles.formBox}>
+                    <h2 className={`sectionTitle ${styles.title}`}>
+                        Стати партнером
+                    </h2>
+                    <PartnerForm />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default PartnershipSection;
