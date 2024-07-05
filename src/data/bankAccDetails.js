b3dc7584dd90fd6cf5113c8e3f0a40520c6312c7
Nataliia <natalabojko@gmail.com>
2024-07-05 09:20:55 +0300

    mobile Accordio bugs

--- src/data/bankAccDetails.js
+++ src/data/bankAccDetails.js
@@ -1,10 +1,12 @@
 export const bankAccDetails = [
   {
     title: "Monobank",
-    href: "https://send.monobank.ua/7fd1Nqu2ey",
+    href: "https://send.monobank.ua/jar/2AfuuyePgy",
   },
   {
     title: "Privat bank",
     href: "https://www.privat24.ua/send/clqp6",
   },
 ];
+
+// href: "https://send.monobank.ua/7fd1Nqu2ey",
