3b9438323b8896e726de5c080ca280e089a4ea68
Nataliia <natalabojko@gmail.com>
2024-07-29 17:11:59 +0300

    changedContactsAddr

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,34 +1,33 @@
 export const contactsData = [
-    {
-        id: 1,
-        src: "map-pin-outline",
-        href: "https://maps.app.goo.gl/Rh6PxCxxDbAd3p7q6",
-        text: "вул. Княжий Затон, 21, м. Київ, Україна",
-        textEn: "21, Knyazhyi Zaton street, Kyiv, Ukraine"
-    },
-    {
-        id: 2,
-        src: "phone-outline",
-        href: "tel:+380971775682",
-        text: "+380 (97) 177 - 56 - 82",
-    },
-    {
-        id: 3,
-        src: "mail-outline",
-        href: "mailto:pvfond.info@gmail.com",
-        text: "pvfond.info@gmail.com",
-    },
-    {
-        id: 4,
-        src: "telegram-outline",
-        href: "https://t.me/pvfond_contact",
-        text: "@pvfond_contact",
-    },
-    {
-        id: 5,
-        src: "instagram-outline",
-        href: "https://www.instagram.com/pvfond/",
-        text: "https://www.instagram.com/pvfond",
-    },
+  {
+    id: 1,
+    src: "map-pin-outline",
+    href: "https://maps.app.goo.gl/Rh6PxCxxDbAd3p7q6",
+    text: "просп. Науки, 21, м. Київ, Україна",
+    textEn: "21, Nauky Ave., Kyiv, Ukraine",
+  },
+  {
+    id: 2,
+    src: "phone-outline",
+    href: "tel:+380971775682",
+    text: "+380 (97) 177 - 56 - 82",
+  },
+  {
+    id: 3,
+    src: "mail-outline",
+    href: "mailto:pvfond.info@gmail.com",
+    text: "pvfond.info@gmail.com",
+  },
+  {
+    id: 4,
+    src: "telegram-outline",
+    href: "https://t.me/pvfond_contact",
+    text: "@pvfond_contact",
+  },
+  {
+    id: 5,
+    src: "instagram-outline",
+    href: "https://www.instagram.com/pvfond/",
+    text: "https://www.instagram.com/pvfond",
+  },
 ];
-
